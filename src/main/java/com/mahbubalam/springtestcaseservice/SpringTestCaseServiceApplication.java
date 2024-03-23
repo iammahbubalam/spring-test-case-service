42cd3415cb2a7b325eee49865093ca01142a5527
iammahbubalam <mdmahbubnirjon@gmail.com>
2024-03-24 05:34:23 +0600

    Initial commit

---
+++ src/main/java/com/mahbubalam/springtestcaseservice/SpringTestCaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mahbubalam.springtestcaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestCaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahbubalam/springtestcaseservice/SpringTestCaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahbubalam.springtestcaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
